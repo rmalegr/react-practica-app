4a2f02e8f79921b6620b06f32d5072810cfd540b
rodrigomalegre@gmail.com <rodrigomalegre@gmail.com>
2022-04-25 12:34:07 -0300

    First commuit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
 
-function App() {
+export const App = () => {
+  console.log('Hola mundo ')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     
+    <Fragment>
+      <h1>Hola mundo </h1>
+      <h2> fecha  { new Date().toLocaleDateString()}</h2>
+      <p>Escribimos algo facil </p>
+
+
+    </Fragment>
+      
+  )
 }
 
-export default App;
+export default App
+
+
